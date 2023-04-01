a690abbad6be7f0beb5b36c4bc09b1cc360b012d
Roman Kolmakov <smtexx@hotmail.com>
2023-04-01 13:12:54 +0200

    Add next update date to selected data

--- src/processing/selectCurrenciesData.js
+++ src/processing/selectCurrenciesData.js
@@ -1,7 +1,9 @@
 export function selectCurrenciesData(data) {
-  const { time_last_update_unix, rates } = data;
+  const { time_last_update_unix, time_next_update_unix, rates } =
+    data;
   return {
     updated: time_last_update_unix * 1000,
+    nextUpdate: time_next_update_unix * 1000,
     rates,
   };
 }
