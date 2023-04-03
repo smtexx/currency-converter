cbaf2a2c4b61339977a1765120c8538bdfb1e79d
Roman Kolmakov <smtexx@hotmail.com>
2023-04-03 10:46:46 +0200

    Add logic to static component

--- src/components/UserRates/UserRates.jsx
+++ src/components/UserRates/UserRates.jsx
@@ -2,20 +2,21 @@ import { MdDoubleArrow, MdClear } from 'react-icons/md';
 import { FiAlertCircle } from 'react-icons/fi';
 import { setClass } from '../../lib/helpers';
 import s from './UserRates.module.scss';
+import { transformUserRates } from '../../processing/transformUserRates';
+import { actions } from '../../screens/Converter/reducer';
 
-export default function UserRates() {
-  const rates = {
-    EUR: {
-      to: 'RSD',
-      rate: 1.4,
-      value: 117.5,
-    },
-    RSD: {
-      to: 'RUB',
-      rate: 0.6,
-      value: 45,
-    },
-  };
+export default function UserRates({ rates, dispatch }) {
+  const userRates = transformUserRates(rates);
+
+  function handleDelete(from, to) {
+    dispatch({
+      type: actions.deleteUserRate,
+      payload: {
+        from,
+        to,
+      },
+    });
+  }
 
   return (
     <section className={setClass([[s.wrapper], ['cm-block']])}>
@@ -26,22 +27,27 @@ export default function UserRates() {
         <h2 className={s.title}>Сохраненные курсы</h2>
       </header>
       <ul className={s.rates}>
-        {Object.entries(rates).map(([from, { to, value }]) => (
-          <li className={s.rate}>
-            <span className={s.rateFrom}>{from}</span>
-            <span className={s.rateArrow}>
-              <MdDoubleArrow />
-            </span>
-            <span className={s.rateTo}>{to}</span>
-            <span className={s.rateArrow}>
-              <MdDoubleArrow />
-            </span>
-            <span className={s.rateValue}>{value}</span>
-            <button className={s.closeButton}>
-              <MdClear />
-            </button>
-          </li>
-        ))}
+        {userRates.length === 0
+          ? 'Сохраненные курсы валют отсутствуют'
+          : userRates.map(({ from, to, value }) => (
+              <li className={s.rate} key={`${from}-${to}`}>
+                <span className={s.rateFrom}>{from}</span>
+                <span className={s.rateArrow}>
+                  <MdDoubleArrow />
+                </span>
+                <span className={s.rateTo}>{to}</span>
+                <span className={s.rateArrow}>
+                  <MdDoubleArrow />
+                </span>
+                <span className={s.rateValue}>{value}</span>
+                <button
+                  className={s.closeButton}
+                  onClick={() => handleDelete(from, to)}
+                >
+                  <MdClear />
+                </button>
+              </li>
+            ))}
       </ul>
     </section>
   );
