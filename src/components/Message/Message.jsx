ddce5039227d6b903269e09d804a0fd7748c1b1c
Roman Kolmakov <smtexx@hotmail.com>
2023-04-05 13:09:05 +0200

    Add new type of messages - reference

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,12 @@
 import { BsCloudDownload } from 'react-icons/bs';
 import { BiErrorCircle } from 'react-icons/bi';
+import { MdClear } from 'react-icons/md';
 import { messages } from '../../screens/Converter/initialState';
 import s from './Message.module.scss';
+import { actions } from '../../screens/Converter/reducer';
+import { setClass } from '../../lib/helpers';
 
-export default function Message({ type, text }) {
+export default function Message({ type, text, dispatch }) {
   // Иконки используемые для статусов
   const icons = [<BsCloudDownload />, <BiErrorCircle />];
 
@@ -12,8 +15,23 @@ export default function Message({ type, text }) {
     return null;
   }
 
+  function handleCloseMessage() {
+    dispatch({
+      type: actions.changeMessage,
+      payload: {
+        type: messages.ready,
+        text: null,
+      },
+    });
+  }
+
   return (
-    <div className={s.wrapper}>
+    <div
+      className={setClass([
+        [s.wrapper],
+        [s.reference, type === messages.reference],
+      ])}
+    >
       <div
         className={`${s.icon} ${
           type === messages.loading ? s.animate : ''
@@ -22,6 +40,16 @@ export default function Message({ type, text }) {
         {icons[type]}
       </div>
       <p className={s.text}>{text}</p>
+      {type === messages.reference && (
+        <button
+          className={s.closeButton}
+          aria-label="Закрыть сообщение"
+          title="Закрыть сообщение"
+          onClick={handleCloseMessage}
+        >
+          <MdClear />
+        </button>
+      )}
     </div>
   );
 }
